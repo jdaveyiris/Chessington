64a9e6dae3a298fa2bc71faaa49c8c960aede6b9
Jim Davey <jim.davey@iris.co.uk>
2021-12-02 17:24:21 +0000

    Red-07 Complete.

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -10,7 +10,42 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square newPos;
+            Square currentPos = board.FindPiece(this);
+            var availablePos = new List<Square>();
+
+            for (int i = -1; i < 2; i++)
+            {
+                if (i != 0)
+                {
+                    // diag  up
+                    if ((currentPos.Row + i) < 8 && (currentPos.Col + i) < 8)
+                    {
+                        newPos = Square.At(currentPos.Row + i, currentPos.Col + i);
+                        availablePos.Add(newPos);
+                    }
+                    //diag  down
+                    if ((currentPos.Row + i) < 8 && (currentPos.Col - i) >= 0)
+                    {
+                        newPos = Square.At(currentPos.Row + i, currentPos.Col - i);
+                        availablePos.Add(newPos);
+                    }
+                   
+                    // left and right
+                    if (currentPos.Row + i >= 0 && (currentPos.Row + i) < 8)
+                    {
+                        newPos = Square.At(currentPos.Row + i, currentPos.Col);
+                        availablePos.Add(newPos);
+                    }
+                    //up and down
+                    if (currentPos.Col + i >= 0 && (currentPos.Col + i) < 8)
+                    {
+                        newPos = Square.At(currentPos.Row, currentPos.Col + i);
+                        availablePos.Add(newPos);
+                    }
+                }
+            }
+            return availablePos;
         }
     }
 }
