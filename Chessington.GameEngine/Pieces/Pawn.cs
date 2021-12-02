637b2b2c30407fafd96e3cd9f0efacf27739825f
Jim Davey <jim.davey@iris.co.uk>
2021-12-02 11:34:16 +0000

    Red-01 complete

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -10,7 +10,32 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            // set of rules to allow pawn to move 
+            // find piece, show where it can move to, return that info to the board to be displayed
+            // movement itself not included here
+            // must admit I did use your code shown this morning to make this. 
+
+            Square newPos;
+
+            Square currentPos = board.FindPiece(this);
+            //'this' is referring to the current Pawn selected?
+            var availablePos = new List<Square>();
+
+            if (Player == Player.White)
+            {
+                newPos = Square.At(currentPos.Row - 1, currentPos.Col);
+
+                availablePos.Add(newPos);
+            }
+
+            if (Player == Player.Black)
+            {
+                newPos = Square.At(currentPos.Row + 1, currentPos.Col);
+
+                availablePos.Add(newPos);
+            }
+
+            return availablePos;
         }
     }
 }
