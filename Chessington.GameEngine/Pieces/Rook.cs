d75daa73b656d7d44068558f55cd2b3757ce1f09
Jim Davey <jim.davey@iris.co.uk>
2021-12-02 14:02:50 +0000

    Red-03 Complete

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -10,7 +11,86 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square newPos;
+            Square currentPos = board.FindPiece(this);
+            var availablePos = new List<Square>();
+
+
+            if (Player == Player.White)
+            {
+                newPos = Square.At(currentPos.Row - 1, currentPos.Col);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row - 2, currentPos.Col);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row - 3, currentPos.Col);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row - 4, currentPos.Col);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row - 5, currentPos.Col);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row - 6, currentPos.Col);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row - 7, currentPos.Col);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row + 1, currentPos.Col);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row + 2, currentPos.Col);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row + 3, currentPos.Col);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row + 4, currentPos.Col);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row + 5, currentPos.Col);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row + 6, currentPos.Col);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row + 7, currentPos.Col);
+                availablePos.Add(newPos);
+
+                newPos = Square.At(currentPos.Row, currentPos.Col + 1);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row, currentPos.Col + 2);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row, currentPos.Col + 3);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row, currentPos.Col + 4);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row, currentPos.Col + 5);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row, currentPos.Col + 6);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row, currentPos.Col + 7);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row, currentPos.Col - 1);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row, currentPos.Col - 2);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row, currentPos.Col - 3);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row, currentPos.Col - 4);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row, currentPos.Col - 5);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row, currentPos.Col - 6);
+                availablePos.Add(newPos);
+                newPos = Square.At(currentPos.Row, currentPos.Col - 7);
+                availablePos.Add(newPos);
+
+                // Was trying to figure out how to pass a range through  ¯\_(ツ)_/¯
+                //newPos = Square.At(currentPos.Row, currentPos.Col + Range(1,7));
+                //availablePos.Add(newPos);
+                //newPos = Square.At(currentPos.Row, currentPos.Col - Range(1,7));
+                //availablePos.Add(newPos);
+            }
+
+
+
+            return availablePos;
+        }
+
+        private int Range(int v1, int v2)
+        {
+            throw new NotImplementedException();
         }
     }
 }
