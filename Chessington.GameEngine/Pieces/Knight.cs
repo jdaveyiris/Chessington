f397d4217bf07bcbd4916e8b2edfdca11b9b9bba
Jim Davey <jim.davey@iris.co.uk>
2021-12-02 17:04:59 +0000

    Red-06 Complete.

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -10,7 +10,57 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square newPos;
+            Square currentPos = board.FindPiece(this);
+            var availablePos = new List<Square>();
+
+            //needs cleaning up (refactoring).
+
+                    if ((currentPos.Row + 1) < 8 && (currentPos.Col + 2) < 8)
+                    {
+                        newPos = Square.At(currentPos.Row + 1, currentPos.Col + 2);
+                        availablePos.Add(newPos);
+                    }
+                    if ((currentPos.Row + 2) < 8 && (currentPos.Col + 1) < 8)
+                    {
+                        newPos = Square.At(currentPos.Row + 2, currentPos.Col + 1);
+                        availablePos.Add(newPos);
+                    }
+
+                    if ((currentPos.Row + 1) < 8 && (currentPos.Col - 2) >= 0)
+                    {
+                        newPos = Square.At(currentPos.Row + 1, currentPos.Col - 2);
+                        availablePos.Add(newPos);
+                    }
+                    if ((currentPos.Row + 2) < 8 && (currentPos.Col - 1) >= 0)
+                    {
+                        newPos = Square.At(currentPos.Row + 2, currentPos.Col - 1);
+                        availablePos.Add(newPos);
+                    }
+
+                    if ((currentPos.Row - 1) >= 0 && (currentPos.Col + 2) < 8)
+                    {
+                        newPos = Square.At(currentPos.Row - 1, currentPos.Col + 2);
+                        availablePos.Add(newPos);
+                    }
+                    if ((currentPos.Row - 2) >= 0 && (currentPos.Col + 1) < 8)
+                    {
+                        newPos = Square.At(currentPos.Row - 2, currentPos.Col + 1);
+                        availablePos.Add(newPos);
+                    }
+
+                    if ((currentPos.Row - 1) >= 0 && (currentPos.Col - 2) >= 0)
+                    {
+                        newPos = Square.At(currentPos.Row - 1, currentPos.Col - 2);
+                        availablePos.Add(newPos);
+                    }
+                    if ((currentPos.Row - 2) >= 0 && (currentPos.Col - 1) >= 0)
+                    {
+                        newPos = Square.At(currentPos.Row - 2, currentPos.Col - 1);
+                        availablePos.Add(newPos);
+                    }
+              
+            return availablePos;
         }
     }
 }
